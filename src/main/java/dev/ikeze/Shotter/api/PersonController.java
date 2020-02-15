106a80f357361c017ee408db2d6a4ae097aaf842
iykekings <iykekings36@gmail.com>
2020-02-15 11:25:45 +0100

    feat: getPersonById, getAllPeople and addNewPerson

--- src/main/java/dev/ikeze/Shotter/api/PersonController.java
+++ src/main/java/dev/ikeze/Shotter/api/PersonController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("api/v1/person")
 @RestController
@@ -25,5 +26,11 @@ public class PersonController {
     @GetMapping
     public List<Person> getAllPeople() {
         return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id) {
+        return personService.getPersonById(id)
+                .orElse(null);
     }
 }
--- src/main/java/dev/ikeze/Shotter/dao/PersonDao.java
+++ src/main/java/dev/ikeze/Shotter/dao/PersonDao.java
@@ -3,6 +3,7 @@ package dev.ikeze.Shotter.dao;
 import dev.ikeze.Shotter.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonDao {
@@ -13,4 +14,10 @@ public interface PersonDao {
     }
 
     List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
 }
