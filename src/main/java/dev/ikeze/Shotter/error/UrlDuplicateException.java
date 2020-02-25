9cbd6374c8fbd3b4e01c4f923fc4155a5b9cc68c
iykekings <iykekings36@gmail.com>
2020-02-25 01:57:04 +0100

    ignore: fix warnings

--- src/main/java/dev/ikeze/Shotter/error/UrlDuplicateException.java
+++ src/main/java/dev/ikeze/Shotter/error/UrlDuplicateException.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.CONFLICT)
 public class UrlDuplicateException extends RuntimeException {
 
+  private static final long serialVersionUID = -3670278356021712338L;
+
   public UrlDuplicateException(String directory) {
     super("Url is a duplicate : " + directory);
   }
--- src/main/java/dev/ikeze/Shotter/error/UrlNotFoundException.java
+++ src/main/java/dev/ikeze/Shotter/error/UrlNotFoundException.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class UrlNotFoundException extends RuntimeException {
-  // private static final long serialVersionUID = 1L;
+
+  private static final long serialVersionUID = 1700014759880456200L;
 
   public UrlNotFoundException(String id) {
     super("Url not found : " + id);
