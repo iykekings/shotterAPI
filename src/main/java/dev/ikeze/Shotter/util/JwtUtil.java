c7de8221b69fd55e84d1f05d374698863f404540
iykekings <iykekings36@gmail.com>
2020-02-27 07:18:28 +0100

    set jwt secret in application.properties

--- src/main/java/dev/ikeze/Shotter/util/JwtUtil.java
+++ src/main/java/dev/ikeze/Shotter/util/JwtUtil.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,8 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Service
 public class JwtUtil {
 
-  private String SECRET_KEY = "secret";
+  @Value("${jwt.secret}")
+  private String SECRET_KEY;
 
   public String extractUsername(String token) {
     return extractClaim(token, Claims::getSubject);
