67025fefd4972485dc5ae8c0974d5c76229a12ec
iykekings <iykekings36@gmail.com>
2020-02-27 06:59:20 +0100

    add helper methods for JWT

---
+++ src/main/java/dev/ikeze/Shotter/model/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package dev.ikeze.Shotter.model;
+
+import java.io.Serializable;
+
+public class AuthenticationResponse implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private final String token;
+
+  public AuthenticationResponse(String token) {
+    this.token = token;
+  }
+
+  public String getToken() {
+    return token;
+  }
+}
