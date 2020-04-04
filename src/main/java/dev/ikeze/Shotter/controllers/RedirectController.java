72411369df97876f11c940b9163c796238891959
iykekings <iykekings36@gmail.com>
2020-04-04 14:11:06 +0100

    redirect unknown urls to frontend's 404

--- src/main/java/dev/ikeze/Shotter/controllers/RedirectController.java
+++ src/main/java/dev/ikeze/Shotter/controllers/RedirectController.java
@@ -1,26 +1,31 @@
 package dev.ikeze.Shotter.controllers;
 
+import dev.ikeze.Shotter.repos.UrlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import dev.ikeze.Shotter.model.Url;
-import dev.ikeze.Shotter.services.UrlService;
-
 @RequestMapping(value = "r")
 @RestController
 public class RedirectController {
+
   @Autowired
-  private UrlService urlService;
+  private UrlRepository urlRepository;
+
+  @Value("${ikeze.frontendUrl}")
+  private String frontendUrl;
 
   @GetMapping(value = "{directory}")
   public ModelAndView redirectToNewUrl(@PathVariable String directory) {
-    Url fullUrl = urlService.findByDirectory(directory);
-    fullUrl.setClicks(fullUrl.getClicks() + 1);
-    urlService.updateById(fullUrl.getId(), fullUrl);
-    return new ModelAndView(String.format("redirect:%s", fullUrl.getRedirect()));
+    return urlRepository.findByDirectory(directory).map(x -> {
+      x.setClicks(x.getClicks() + 1);
+      urlRepository.save(x);
+      return new ModelAndView(String.format("redirect:%s", x.getRedirect()));
+    }).orElseGet(() -> new ModelAndView("redirect:" + frontendUrl + "/404"));
   }
+
 }
